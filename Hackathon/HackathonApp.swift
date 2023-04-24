4831cdd1622487532f659ad0117a7f82aa99a4b4
OscarValdes1997 <313227743@pcpuma.acatlan.unam.mx>
2023-04-24 12:02:48 -0700

    Initial Commit

---
+++ Hackathon/HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonApp.swift
+//  Hackathon
+//
+//  Created by CEDAM01 on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
